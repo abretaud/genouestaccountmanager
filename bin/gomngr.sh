753c6605d25ebdb197994b60ae556d40dd6ab020
Olivier Sallou <olivier.sallou@irisa.fr>
2014-12-04 18:06:15 +0100

    add dir as param

--- bin/gomngr.sh
+++ bin/gomngr.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+if [ "a$1" == "a" ]; then
+ echo "Missing script directory parameter"
+ exit 1
+fi
+
 if [ -e /tmp/gomngr.lock ]; then
-  exit(1)
+  exit 1
 fi
 
 touch /tmp/gomngr.lock
 
-ls /tmp/*.update | sort -n -t _ -k 2 > /tmp/gomngr.list
+ls $1/*.update | sort -n -t _ -k 2 > /tmp/gomngr.list
 
 while read p; do
   $p
