70f3d81aadbfa40ff0ba1ab7ad8913923447d96f
Olivier Sallou <olivier.sallou@irisa.fr>
2014-12-05 13:16:49 +0100

    add script to check expiration

---
+++ test_expiration.js
@@ -0,0 +1,77 @@
+var CONFIG = require('config');
+var goldap = require('./routes/goldap.js');
+var notif = require('./routes/notif.js');
+
+var monk = require('monk'),
+    db = monk(CONFIG.mongo.host+':'+CONFIG.mongo.port+'/'+CONFIG.general.db),
+    users_db = db.get('users');
+
+var nodemailer = require('nodemailer');
+var smtpTransport = require('nodemailer-smtp-transport');
+var MAIL_CONFIG = CONFIG.mail;
+var transport = null;
+
+
+if(MAIL_CONFIG.host !== 'fake') {
+  if(MAIL_CONFIG.user !== undefined && MAIL_CONFIG.user !== '') {
+  transport = nodemailer.createTransport(smtpTransport({
+    host: MAIL_CONFIG.host, // hostname
+    secureConnection: MAIL_CONFIG.secure, // use SSL
+    port: MAIL_CONFIG.port, // port for secure SMTP
+    auth: {
+        user: MAIL_CONFIG.user,
+        pass: MAIL_CONFIG.password
+    }
+  }));
+  }
+  else {
+  transport = nodemailer.createTransport(smtpTransport({
+    host: MAIL_CONFIG.host, // hostname
+    secureConnection: MAIL_CONFIG.secure, // use SSL
+    port: MAIL_CONFIG.port, // port for secure SMTP
+  }));
+
+  }
+}
+
+function timeConverter(tsp){
+  var a = new Date(tsp);
+  var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+  var year = a.getFullYear();
+  var month = months[a.getMonth()];
+  var date = a.getDate();
+  var hour = a.getHours();
+  var min = a.getMinutes();
+  var sec = a.getSeconds();
+  var time = date + ',' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
+  return time;
+}
+
+// Find users expiring in less then 2 month
+users_db.find({duration: {$lt: (new Date().getTime() + 1000*3600*24*60)}},{uid: 1}, function(err, users){
+  console.log(users);
+  var mail_sent = 0;
+  for(var i=0;i<users.length;i++){
+    var user = users[i];
+    var msg_activ = CONFIG.message.expiration.join("\n").replace("#EXPIRE", timeConverter(user.expiration)).replace('#UID#', user.uid).replace('#PASSWORD#', user.password).replace('#IP#', user.ip)+"\n"+CONFIG.message.footer.join("\n");
+    var msg_activ_html = CONFIG.message.expiration.join("<br/>").replace("#EXPIRE", timeConverter(user.expiration)).replace('#UID#', user.uid).replace('#PASSWORD#', user.password).replace('#IP#', user.ip)+"<br/>"+CONFIG.message.footer.join("<br/>");
+    var mailOptions = {
+      from: MAIL_CONFIG.origin, // sender address
+      to: user.email, // list of receivers
+      subject: 'Genouest account expiration', // Subject line
+      text: msg_activ, // plaintext body
+      html: msg_activ_html // html body
+    };
+    transport.sendMail(mailOptions, function(error, response){
+        if(error){
+          console.log(error);
+        }
+        mail_sent++;
+        if(mail_sent == users.length) {
+          process.exit(code=0);
+        }
+    });
+
+  }
+
+});
