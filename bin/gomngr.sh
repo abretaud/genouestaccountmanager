e0d36e7c40e6b6ae83783058fac316208f015fbb
Olivier Sallou <osallou@gomngr.genouest.org>
2017-01-25 18:59:49 +0100

    fix callback

--- bin/gomngr.sh
+++ bin/gomngr.sh
@@ -20,7 +20,9 @@ ls $1/*.update | sort -n -t . -k 2 > /tmp/gomngr.list
 
 while read p; do
   $p &> $p.log
-  curl "$2/log/status/$p/$?"
+  EXITCODE=$?
+  filename=$(basename $p)
+  curl -v "$2/log/status/$filename/$EXITCODE"
   mv $p $p.done
 done </tmp/gomngr.list
 
