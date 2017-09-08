036d940a9835f05079d1308e50e7644a33dfb6b2
Olivier Sallou <olivier.sallou@irisa.fr>
2017-09-08 12:51:25 +0200

    change logger format

--- app.js
+++ app.js
@@ -21,8 +21,7 @@ var plugin = require('./routes/plugin');
 var CONFIG = require('config');
 
 var app = express();
-
-
+app.use(logger('combined'));
 // view engine setup
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
@@ -30,7 +29,7 @@ app.set('view engine', 'hjs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
+//app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
